0b6a67050d425560652385d4e4f2b468d6011c6c
BarminaSK <barminask@mail.ru>
2025-09-17 01:05:13 +0500

    Task1

---
+++ Tyuiu.BarminaSK.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BarminaSK.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "София";
+            int age = 17;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ". Мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
