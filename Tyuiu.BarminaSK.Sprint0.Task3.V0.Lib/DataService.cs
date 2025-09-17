f0e1bf2fee9ed8d3c0396dd412bf79ae6a79e2fe
BarminaSK <barminask@mail.ru>
2025-09-17 05:02:52 +0500

    Task3

---
+++ Tyuiu.BarminaSK.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BarminaSK.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        //Реализация метода Sum
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
